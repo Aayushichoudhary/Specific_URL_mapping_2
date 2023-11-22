3ff700e297d29567837ace55af192271d5bc22cd
aayushi <ayushichoudhary@gmail.com>
2023-11-22 14:32:05 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def rohit(request):
+    return render(request, 'rohit.html')
+
+def bumrah(request):
+    return HttpResponse('<center><h1>Boom Boom Bumrah</h1></center>')
